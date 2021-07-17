34915959b7f5cff809d4d410cbf2f397e5f2a199
T25-design <tarunyadav2532@gmail.com>
2021-07-17 15:16:25 +0530

    Order-Online-App

--- app/src/main/java/com/example/OrderOnline/SignIn.java
+++ app/src/main/java/com/example/OrderOnline/SignIn.java
@@ -1,7 +1,5 @@
 package com.example.OrderOnline;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignIn extends AppCompatActivity {
 
